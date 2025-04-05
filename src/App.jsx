d68b1e60a9302043330ff5e36d196884d32e0c86
yesongjoo <yesong.joo7@gmail.com>
2025-04-05 12:53:32 -0400

    프로젝트 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
 import WeatherInfo from "./component/WeatherInfo";
 import CityButtons from "./component/CityButtons";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import "./App.css";
 
+const OPENWEATHER_API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const cities = ["seoul", "london", "sydney", "hanoi"];
+
 function App() {
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -16,10 +22,12 @@ function App() {
   };
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=01c89473860f305743dca34a3b6c5842`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -33,10 +41,34 @@ function App() {
   return (
     <>
       <div className="background">
-        <div className="container">
-          <WeatherInfo weather={weather} />
-          <CityButtons />
-        </div>
+        {loading ? (
+          <div className="container">
+            <ClipLoader
+              color="#fc39ae"
+              loading={loading}
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        ) : (
+          <div className="container">
+            <ClipLoader
+              color="#fc39ae"
+              loading={loading}
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+            <WeatherInfo weather={weather} />
+            <CityButtons
+              cities={cities}
+              setWeather={setWeather}
+              getCurrentLocation={getCurrentLocation}
+              setLoading={setLoading}
+            />
+          </div>
+        )}
       </div>
     </>
   );
--- src/component/CityButtons.jsx
+++ src/component/CityButtons.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import "./CityButtons.css";
+const OPENWEATHER_API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-const CityButtons = () => {
+const CityButtons = ({
+  cities,
+  setWeather,
+  getCurrentLocation,
+  setLoading,
+}) => {
+  const getWeatherByCity = async (city) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHER_API_KEY}`;
+    setLoading(true);
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  };
+
   return (
     <div className="buttons">
-      <button className="city-button">Current Location</button>
-      <button className="city-button">Seoul</button>
-      <button className="city-button">Lodon</button>
-      <button className="city-button">Sydney</button>
-      <button className="city-button">Hanoi</button>
+      <button className="city-button" onClick={getCurrentLocation}>
+        Current Location
+      </button>
+      {cities.map((city) => (
+        <button
+          key={city}
+          className="city-button"
+          onClick={() => getWeatherByCity(city)}
+        >
+          {city}
+        </button>
+      ))}
     </div>
   );
 };
